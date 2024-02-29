cfdf4a86b33cdbb5a4cd74337faf503f21129265
osk <dhtnrud42@gmail.com>
2024-02-29 15:41:07 +0900

    Feat : 멤버DTO 수정

--- IT_MeetUp/src/com/itmeetup/mybatis/MemberDTO.java
+++ IT_MeetUp/src/com/itmeetup/mybatis/MemberDTO.java
@@ -4,16 +4,36 @@
 
 package com.itmeetup.mybatis;
 
+import java.util.Date;
+
 public class MemberDTO
 {
 	private int piMemCode;	// 회원 코드
 	private String piName, piEmail, piImgFile, piId, piPw, piNickname, piSelfment;	// 이름, 이메일, 이미지 파일명, 아이디, 비밀번호, 닉네임, 짧은 한마디
-	private String mMetCode, mSiggCode;	// 모임방식 코드, 시군구 코드
+	private String mJobCode, mMetCode, mSiggCode;	// 직무코드, 모임방식 코드, 시군구 코드
+	private Date mDate;// 등록일자
 	
 	// getter / setter 
+	
 	public int getPiMemCode()
 	{
 		return piMemCode;
+	}
+	public String getmJobCode()
+	{
+		return mJobCode;
+	}
+	public void setmJobCode(String mJobCode)
+	{
+		this.mJobCode = mJobCode;
+	}
+	public Date getmDate()
+	{
+		return mDate;
+	}
+	public void setmDate(Date mDate)
+	{
+		this.mDate = mDate;
 	}
 	public void setPiMemCode(int piMemCode)
 	{
