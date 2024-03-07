92e4b50f58df21926bea0be5ab809016470a5e05
ljh <juju6799@naver.com>
2024-03-07 11:16:22 +0900

    FEAT : 모집 프로젝트 희망기술 성공

--- IT_MeetUp/src/com/itmeetup/mybatis/IWriteFormDAO.java
+++ IT_MeetUp/src/com/itmeetup/mybatis/IWriteFormDAO.java
@@ -1,9 +1,15 @@
 package com.itmeetup.mybatis;
 
+import java.util.List;
+
 public interface IWriteFormDAO
 {
-	// 프로젝트  추가
+	// 프로젝트  입력
 	public Integer addProject(WriteFormDTO dto);
-	// 상세 일정, 역할별 인원수 추가
-	public Integer addperiod(WriteFormDTO dto);				
+	
+	// 상세 일정, 역할별 인원수 입력
+	public Integer addperiod(WriteFormDTO dto);
+	
+	// 프로젝트 희망 기술 입력 
+	public Integer addSkill(List<String> hopeskills);
 }
