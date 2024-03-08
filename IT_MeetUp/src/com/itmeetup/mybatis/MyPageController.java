15ac4f6c1c068bcb884899fe1aebf91dff5a8c3a
mjh <gd19973@naver.com>
2024-03-08 09:34:57 +0900

    Style: blockController 이름 수정 MyPageController 로

--- IT_MeetUp/src/com/itmeetup/mybatis/MyPageController.java
+++ IT_MeetUp/src/com/itmeetup/mybatis/MyPageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class BlockController
+public class MyPageController
 {
 	// 주요 속성 구성
 	@Autowired
